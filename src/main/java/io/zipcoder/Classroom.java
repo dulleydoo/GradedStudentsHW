b7184e0f1ec0a5f6d56ea775278217a071b15c9c
dulleydoo <karem.jabar2015@gmail.com>
2022-10-03 20:39:01 -0400

    Done

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,85 @@
 package io.zipcoder;
 
+import java.util.*;
+
 public class Classroom {
+    public Student[] students;
+    public Classroom() {
+        this.students = new Student[30];
+    }
+    public Classroom(Student[] students) {
+        this.students = students;
+    }
+    public Classroom(int maxNumberOfStudents) {
+        this.students = new Student[maxNumberOfStudents];
+    }
+    public Double getAverageExamScore() {
+        Double sum = 0.0;
+        for (Student student: students) {
+            sum += student.getAverageExamScore();
+        }
+        return sum / students.length;
+    }
+    public void addStudent(Student student) {
+        ArrayList<Student> newStudentList = new ArrayList<>(Arrays.asList(students));
+        newStudentList.add(student);
+        students = newStudentList.toArray(new Student[0]);
+    }
+    public void removeStudent(String firstName, String lastName) {
+        // removal without leaving null values
+        ArrayList<Student> newStudentList = new ArrayList<>(Arrays.asList(students));
+        for (int i = 0; i < newStudentList.size(); i++) {
+            if (newStudentList.get(i).getFirstName().equals(firstName) &&
+                    newStudentList.get(i).getLastName().equals(lastName)) {
+                newStudentList.remove(newStudentList.get(i));
+                i--;
+            }
+        }
+        students = newStudentList.toArray(new Student[0]);
+    }
+
+    public Student[] getStudentsByScore() {
+        ArrayList<Student> newStudentList = new ArrayList<>(Arrays.asList(students));
+        // descending avg exam, then sorted by last name
+        Comparator<Student> byExamScore = Comparator.comparing(Student::getAverageExamScore);
+        Comparator<Student> byLastName = Comparator.comparing(Student::getLastName);
+        newStudentList.sort(byExamScore.reversed().thenComparing(byLastName));
+
+        return newStudentList.toArray(new Student[0]);
+    }
+
+    public HashMap<Student, String> getGradeBook() {
+        HashMap<Student, String> gradeBook = new HashMap<>();
+        Student[] sortedStudents = getStudentsByScore();
+        int numStudents = sortedStudents.length;
+
+        for (int i = 0; i < numStudents-1; i++) {
+            for (int j = i+1; j < numStudents; j++) {
+                if (sortedStudents[i].getAverageExamScore() > sortedStudents[j].getAverageExamScore()) {
+                    Double percentile = 100.0*(numStudents-j)/(double)numStudents;
+                    String grade = getGradeByPercentile(percentile);
+                    gradeBook.put(sortedStudents[i], grade);
+                    break;
+                }
+            }
+        }
+        // lowest score always gets an F
+        gradeBook.put(sortedStudents[numStudents-1], "F");
+
+        return gradeBook;
+    }
+    public String getGradeByPercentile(Double percentile) {
+        if (percentile.isNaN() || percentile.isInfinite()) return "Nan";
+        if (percentile >= 100.0 || percentile < 0.0) return "Out of Bounds";
+        if (percentile >= 90.0) return "A";
+        if (percentile >= 71.0) return "B";
+        if (percentile >= 50.0) return "C";
+        if (percentile >= 11.0) return "D";
+        if (percentile >= 0.0) return "F";
+        return "N/A";
+    }
+    public Student[] getStudents() {
+        return students;
+    }
+
 }
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,71 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+    String firstName;
+    String lastName;
+    ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<Double>(Arrays.asList(examScores));
+    }
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getExamScores() {
+        String examScoresStr = "Exam Scores:";
+        int numScores = getNumberOfExamsTaken();
+        for (int i = 0; i < numScores; i++) {
+            examScoresStr += String.format("\n\tExam %d -> %.0f",
+                    i+1,  examScores.get(i));
+        }
+        return examScoresStr;
+    }
+    public void setExamScores(int examNumber, double newScore) {
+        examScores.set(examNumber-1, newScore);
+    }
+    public void addExamScore(double examScore) {
+        examScores.add(examScore);
+    }
+    public Double getAverageExamScore() {
+        // we could check here for non-zero examScores size
+        Double sum = examScores.stream().reduce(Double::sum).get();
+        return sum/getNumberOfExamsTaken();
+    }
+    @Override
+    public String toString() {
+        return "Student Name: " + firstName + " " + lastName +
+                (String.format("\n> Average Score: %.0f", getAverageExamScore()) + "\n> ") +
+                getExamScores();
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Double> getExamScoresAsList() {
+        return examScores;
+    }
+
+    public Integer getNumberOfExamsTaken() {
+        return examScores.size();
+    }
 }
